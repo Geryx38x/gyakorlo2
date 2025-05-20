e45ce53e9e1f2645402169f968626a6e3cbde8e3
Petrik Diak <diak@petrik.hu>
2025-05-20 11:38:44 +0200

    first commit

--- feladatok01.py
+++ feladatok01.py
@@ -1,4 +1,9 @@
 gyujt = input("Adja meg mire szeretne gyűjteni: ")
-kutyadb= input("Add meg hány kutyát sétáltatsz a hétvégén: ")
-ora = (kutyadb * 20)/60
-print(ora)
+kutyadb= int(input("Add meg hány kutyát sétáltatsz a hétvégén: "))
+ora = (kutyadb * 20)//60
+perc= (kutyadb * 20)%60
+penzossz= kutyadb*700
+if penzossz > 5000:
+    print(f"Anna {kutyadb} kutyát sétáltatott {ora}ó:{perc}p alatt, ezért {penzossz} Ft-ot kapott. Ez már elég a {gyujt}-re.")
+else:
+    print(f"Anna {kutyadb} kutyát sétáltatott {ora}ó:{perc}p alatt, ezért {penzossz} Ft-ot kapott. Ez még nem elég a {gyujt}-ra/re.")
